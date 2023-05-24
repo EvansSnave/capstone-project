1f48562b876c3c498ce0cb08e5d65c9e5d2c6b05
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-05-24 00:10:29 -0500

    finished nav menu

---
+++ javaScript/main.js
@@ -0,0 +1,14 @@
+let hamburger = document.querySelector(".navigator-menu__burger");
+let menu = document.querySelector(".hidden-menu");
+let closeButton = document.querySelector(".hidden-menu__x-element");
+let body = document.body;
+
+hamburger.addEventListener("click", ()=>{
+  menu.style.display = "block";
+  body.style.overflow = "hidden";
+});
+
+closeButton.addEventListener("click", ()=>{
+  menu.style.display = "none";
+  body.style.overflow = "visible";
+});
