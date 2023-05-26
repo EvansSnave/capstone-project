4e3953d3a45b1206fdc2aad0e85304e41f4872de
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-05-25 19:44:33 -0500

    updated path

--- javaScript/featureSection.js
+++ javaScript/featureSection.js
@@ -1,7 +1,7 @@
 // Dynamic Html feature section
 const games = [
   {
-    image: 'capstone-project\images\gamesCovers\diabloIII.jpg',
+    image: 'images\gamesCovers\diabloIII.jpg',
     title: 'Diablo III',
     releaseDate: 'May 15 2012',
     company: 'Blizzard Entertainment',
